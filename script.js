17aca6e52cc8ae458cfba1cbb35b1366b8429bd7
korisnik9 <mrakcevic@gmail.com>
2021-11-26 19:42:28 +0100

    slider

--- script.js
+++ script.js
@@ -1,2 +1,89 @@
-// https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all
+// https://api.thedogapi.com/v1/breeds
 
+var num = 0;
+var arr = [];
+
+function fetchA() {
+    url = "https://api.thedogapi.com/v1/breeds";
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(res) {
+            return res;
+        })
+        .then(function(res) {
+            loadAll(res);
+        }).then(slider);
+    }
+
+    fetchA();
+
+    function loadAll(data) {
+        for (e of data) {
+            arr.push(e);
+        }
+    }
+    document.addEventListener("load", slider);
+
+function slider() {
+    var animalsParent = document.getElementsByClassName("animals")[0];
+    for (e of arr) {
+        if(arr.indexOf(e) === 14) {
+            break;
+        }
+        let animal = document.createElement("div");
+        animal.classList.add("slider");
+        animal.innerHTML += "<p>Bred for <span class=\"dog\"><i>" + e.bred_for + "</i></span></p>"; console.log(e.bred_for)
+        animal.innerHTML += "<div class=\"image\">" + "<h2>" + e.name + "</h2></div>";
+        animal.childNodes[1].style.backgroundImage = "url(" + e.image.url + ")"; console.log(e.image.url)
+        
+        animalsParent.appendChild(animal);
+        
+    }
+    showSlides(0);
+}
+
+function w() {
+    if (window.innerWidth < 650) {
+        return 1;
+    } else if (window.innerWidth > 650 && window.innerWidth < 960) {
+        return 2;
+    } else if (window.innerWidth > 960) {
+        return 3;
+    }
+}
+
+var windowVariable = w();
+window.onresize = function() {
+     windowVariable = w(); 
+    }
+
+
+function showSlides(num) {
+    for (k = 0; k < dogs.length; k++) {
+        dogs[k].style.display = "none";
+    }
+    for (i = 0; i < windowVariable; i++) {
+        dogs[i + num].style.display = "grid";
+    }
+}
+
+dogs = document.getElementsByClassName("slider");
+document.getElementById("left").addEventListener("click", slideBack);
+document.getElementById("right").addEventListener("click", slideForward);
+
+
+function slideForward() {
+num = num + windowVariable;
+if (num >= dogs.length - windowVariable) {
+    num = 0;
+}
+showSlides(num);
+}
+
+function slideBack() {
+num = num - windowVariable;
+if (num < 0) { num = dogs.length - windowVariable - 1; }
+showSlides(num);
+}
